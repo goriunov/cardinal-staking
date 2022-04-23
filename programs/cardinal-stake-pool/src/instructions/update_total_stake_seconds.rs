8b239a88e29da4ca0c7bc1e650059712c401a724
Giannis Chatziveroglou <gchatz@mit.edu>
2022-04-23 18:05:53 -0400

    extra constraint

--- programs/cardinal-stake-pool/src/instructions/update_total_stake_seconds.rs
+++ programs/cardinal-stake-pool/src/instructions/update_total_stake_seconds.rs
@@ -1,9 +1,14 @@
-use {crate::state::*, anchor_lang::prelude::*};
+use {crate::state::*, anchor_lang::prelude::*, anchor_spl::token::TokenAccount};
 
 #[derive(Accounts)]
 pub struct UpdateTotalStakeSecondsCtx<'info> {
     #[account(mut)]
     stake_entry: Account<'info, StakeEntry>,
+
+    #[account(mut, constraint = user_receipt_mint_token_account.amount > 0
+        && ((stake_entry.stake_mint_claimed && user_receipt_mint_token_account.mint == stake_entry.stake_mint.unwrap()) || (stake_entry.original_mint_claimed && user_receipt_mint_token_account.mint == stake_entry.original_mint))
+        && user_receipt_mint_token_account.owner == stake_entry.key())]
+    user_receipt_mint_token_account: Box<Account<'info, TokenAccount>>,
 
     #[account(mut, constraint = stake_entry.last_staker == last_staker.key())]
     last_staker: Signer<'info>,
